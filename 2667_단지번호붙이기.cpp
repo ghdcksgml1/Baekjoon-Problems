0515d1bf0064d83b2cbf6395fc0e007e8247057f
ghdcksgml1 <ghdcksgml2@naver.com>
2021-05-16 12:35:46 +0900

    2021.05.16

--- 2667_단지번호붙이기.cpp
+++ 2667_단지번호붙이기.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <queue>
+#include <algorithm>
 using namespace std;
-int N;
-char n_arr[26][26];
-queue<int> q;
-vector<int> cnt;
-int num = 0;
+int N; // 지도의 크기
+char n_arr[30][30];
+vector<int> cnt; // 단지별 집의 수
+int num = 0; // 단지의 개수
 void dfs(int x,int y,int numm) {
+	// 지도 범위를 벗어나면 return
 	if (x < 0 || y < 0 || x >= N || y >= N) return;
+	// 지도가 0이 아니면
 	if (n_arr[x][y] != '0') {
-		n_arr[x][y] = '0';
-		cnt[numm]++;
-		dfs(x + 1, y,numm);
-		dfs(x, y + 1, numm);
-		dfs(x - 1, y, numm);
-		dfs(x, y - 1, numm);
+		n_arr[x][y] = '0'; // 방문표시
+		cnt[numm]++; // 단지의 집의 개수 증가
+		dfs(x + 1, y, numm); // 오른쪽
+		dfs(x, y + 1, numm); // 위쪽
+		dfs(x - 1, y, numm); // 왼쪽
+		dfs(x, y - 1, numm); // 아래쪽
 	}
 }
 int main(void) {
@@ -34,6 +36,8 @@ int main(void) {
 		}
 	}
 	cout << num << '\n';
+	// 오름차순으로 정렬
+	sort(cnt.begin(), cnt.end());
 	for (int i = 0; i < cnt.size(); i++) {
 		cout << cnt[i] << '\n';
 	}
