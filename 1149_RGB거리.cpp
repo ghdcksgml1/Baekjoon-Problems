93214cbcef0d76657c874c7d9517231b58ae93b4
ghdcksgml1 <ghdcksgml2@naver.com>
2021-05-19 15:54:09 +0900

    2021.05.19

--- 1149_RGB거리.cpp
+++ 1149_RGB거리.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 using namespace std;
 int N;
-int n_arr[1001][3];
-int dp[1001][3];
+int n_arr[1000][3];
+int dp[1000][3];
 int main(void) {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
@@ -12,15 +12,19 @@ int main(void) {
 			cin >> n_arr[i][j];
 		}
 	}
+	// 초기값
 	dp[0][0] = n_arr[0][0]; dp[0][1] = n_arr[0][1]; dp[0][2] = n_arr[0][2];
+	// 점화식
 	for (int i = 1; i < N; i++) {
 		dp[i][0] = min(dp[i - 1][1], dp[i - 1][2]) + n_arr[i][0];
 		dp[i][1] = min(dp[i - 1][0], dp[i - 1][2]) + n_arr[i][1];
 		dp[i][2] = min(dp[i - 1][0], dp[i - 1][1]) + n_arr[i][2];
 	}
+	// N에서 최솟값 찾기
 	int minn = dp[N-1][0];
 	for (int i = 1; i < N; i++) {
 		minn = min(minn, dp[N-1][i]);
 	}
+	/*************************/
 	cout << minn << '\n';
 }
