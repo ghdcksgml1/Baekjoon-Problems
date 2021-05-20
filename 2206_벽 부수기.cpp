2cb8e3ef3c46c7d84eabfbd4e032d02f94cc5fe7
ghdcksgml1 <ghdcksgml2@naver.com>
2021-05-20 23:31:59 +0900

    2021.05.20

--- 2206_벽 부수기.cpp
+++ 2206_벽 부수기.cpp
@@ -3,58 +3,53 @@
 using namespace std;
 int N, M;
 char graph[1001][1001];
-int visited[1001][1001];
-queue<pair<int, int>> idx_1;
-queue<pair<int,int>> q;
+bool visited[1001][1001][2];
 int dx[4] = { 1,-1,0,0 };
 int dy[4] = { 0,0,1,-1 };
-priority_queue<int, vector<int>, greater<int>> pq;
 int main(void) {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
+
 	cin >> N >> M;
 	for (int i = 0; i < N; i++) {
 		cin >> graph[i];
 	}
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (graph[i][j] == '1') { // 벽이면
-				idx_1.push({ i,j });
+	
+	// bfs
+	queue<pair<pair<int, int>, pair<int, int>>> q;
+	q.push({ {0,0},{1,1} });
+	visited[0][0][1] = true;
+
+	while (!q.empty()) {
+		int x = q.front().first.first;
+		int y = q.front().first.second;
+		int wall = q.front().second.first;
+		int cnt = q.front().second.second;
+		q.pop();
+		// 가장먼저 N x M에 도착한게 최솟값이므로
+		if (x == N - 1 && y == M - 1) {
+			cout << cnt << '\n';
+			return 0;
+		}
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+			// 범위를 벗어나면 continue
+			if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+			// 벽 && 부수기 남은횟수 1
+			if (graph[nx][ny] == '1' && wall == 1) {
+				visited[nx][ny][wall] = true;
+				q.push({ {nx,ny},{wall - 1,cnt + 1} });
 			}
-		}
-	}
-	while (!idx_1.empty()) {
-		auto idx_cur = idx_1.front();
-		idx_1.pop();
-		graph[idx_cur.first][idx_cur.second] = '0';
-		q.push({ 0,0 });
-		visited[0][0] = 1;
-		while (!q.empty()) {
-			auto cur = q.front(); q.pop();
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur.first + dx[dir];
-				int ny = cur.second + dy[dir];
-				if (nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
-				if (graph[nx][ny] == '1' || visited[nx][ny] > 0) continue;
-				visited[nx][ny] = visited[cur.first][cur.second] + 1;
-				q.push({ nx,ny });
+			// 길 && 방문하지않았을때
+			if (graph[nx][ny] == '0' && !visited[nx][ny][wall]) {
+				visited[nx][ny][wall] = true;
+				q.push({ {nx,ny},{wall,cnt + 1} });
 			}
+			// 중요!!
+			// visited에서 true와 false는 단순히 방문했는지 안했는지임
+			// 벽 부수기 남은 횟수와는 관련없음
 		}
-		int vvs = visited[N - 1][M - 1];
-		if (vvs != 0) {
-			pq.push(vvs);
-		}
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				visited[i][j] = 0;
-			}
-		}
-		graph[idx_cur.first][idx_cur.second] = '1';
 	}
-	if (!pq.empty()) {
-		cout << pq.top() << '\n';
-	}
-	else {
-		cout << "-1" << '\n';
-	}
+	cout << "-1" << '\n'; return 0;
 }
