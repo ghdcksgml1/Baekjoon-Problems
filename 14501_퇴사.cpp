3079c8904e8fd3be8d2eebbf18544129d20887c9
ghdcksgml1 <ghdcksgml2@naver.com>
2021-06-11 23:22:56 +0900

    2021.06.11

--- 14501_퇴사.cpp
+++ 14501_퇴사.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
 int N;
-int T[15];
-int P[15];
-int dp[15];
+int T[16];
+int P[16];
+int dp[16];
 void init() {
 	cin.tie(0); cout.tie(0);
 	ios::sync_with_stdio(0);
@@ -11,16 +11,16 @@ void init() {
 int main(void) {
 	init();
 	cin >> N;
-	for (int i = 0; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		cin >> T[i] >> P[i];
 	}
-	for (int i = N; i >= 0; i--) {
-		if (i + T[i] > N) {
+	for (int i = N; i >= 1; i--) {
+		if (i + T[i]-1 > N) {
 			dp[i] = dp[i + 1];
 		}
 		else {
 			dp[i] = max(dp[i + T[i]] + P[i], dp[i + 1]);
 		}
 	}
-	cout << dp[0] << '\n';
+	cout << dp[1] << '\n';
 }
