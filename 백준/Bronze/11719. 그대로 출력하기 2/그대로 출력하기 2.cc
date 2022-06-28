77fceb45ffa8c9123e3beeaa2dc8fc3382e411f8
홍찬희 <ghdcksgml2@naver.com>
2022-06-29 02:34:34 +0900

    [Bronze II] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,26 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s;
+    while(getline(cin,s)){
+        cout<<s<<'\n';
+    }
+
+    return 0;
+}
