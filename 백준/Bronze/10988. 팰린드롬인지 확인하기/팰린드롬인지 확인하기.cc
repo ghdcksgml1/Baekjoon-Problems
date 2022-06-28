2f3efdfaa5f4d21738366c781441a3f307c4957f
홍찬희 <ghdcksgml2@naver.com>
2022-06-29 03:12:28 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,36 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s; cin>>s;
+
+    bool trigger = false;
+    int size = (s.size()/2)-1;
+    for(int i=0;i<=size;i++){
+        if(s[i] != s[s.size()-1-i]){
+            trigger = true;
+            break;
+        }
+    }
+
+    cout<< (trigger?"0":"1")<<'\n';
+
+    return 0;
+}
