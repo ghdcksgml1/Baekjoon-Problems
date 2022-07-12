f074b873851b55d9c12b74550574d1401c61159b
홍찬희 <ghdcksgml2@naver.com>
2022-07-12 18:19:53 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,44 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <deque>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int arr[1001];
+int L[1001];
+int P[1001];
+vector<int> result;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N; cin>>N;
+    int len = 0;
+    int idx;
+    for(int i=0;i<N;i++) {
+        cin>>arr[i];
+        idx = lower_bound(L,L+len,arr[i]) - L;
+        L[idx] = arr[i];
+        P[i] = idx;
+        if(idx == len){
+            len++;
+        }
+    }
+
+    cout<<len<<'\n';
+
+    return 0;
+}
