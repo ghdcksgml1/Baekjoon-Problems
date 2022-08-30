3e02fa4bb34aca4919596987bfae59067217ca61
홍찬희 <ghdcksgml2@naver.com>
2022-08-30 22:31:10 +0900

    [Gold V] Title: A와 B, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.cc
@@ -0,0 +1,39 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string a,b; cin>>a>>b;
+
+    while(true){
+        if(a.length() == b.length()){
+            if(a == b) cout<<"1\n";
+            else cout<<"0\n";
+            break;
+        }
+        if(b[b.length() - 1] == 'A'){
+            b = b.substr(0, b.length() - 1);
+        }else if(b[b.length() - 1] == 'B'){
+            b = b.substr(0, b.length() - 1);
+            reverse(b.begin(), b.end());
+        }
+    }
+
+    return 0;
+}
