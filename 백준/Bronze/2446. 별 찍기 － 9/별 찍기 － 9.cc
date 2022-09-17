e521035f370b65f4db4c53eb635c53250a544be2
홍찬희 <ghdcksgml2@naver.com>
2022-09-17 23:20:03 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.cc
@@ -0,0 +1,39 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N; cin>>N;
+
+    int idx = 0, p = 1;
+    for(int i=0; i<2*N-1; i++){
+        for(int j=0; j<idx;j++){
+            cout<<" ";
+        }
+        for(int j=idx; j<(2*N-1)-idx; j++){
+            cout<<"*";;
+        }
+        cout<<"\n";
+
+        if(idx == N-1) p = -1;
+        idx += p;
+    }
+
+    return 0;
+}
