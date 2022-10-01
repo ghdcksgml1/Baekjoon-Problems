d2bddec822e1159d987d2b72e393f964125c9575
홍찬희 <ghdcksgml2@naver.com>
2022-10-01 23:21:38 +0900

    [Silver IV] Title: 게임을 만든 동준이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2847. 게임을 만든 동준이/게임을 만든 동준이.cc
@@ -0,0 +1,43 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int N; cin>>N;
+
+    vector<int> v;
+    for(int i=0; i<N; i++){
+        int tmp; cin>>tmp;
+        v.push_back(tmp);
+    }
+
+    int cnt = 0;
+    for(int i=v.size()-2; i>=0 ;i--){
+        if(v[i+1] > v[i]) continue;
+        else{
+            int diff = v[i] - v[i+1] + 1;
+            cnt += diff;
+            v[i] -= diff;
+        }
+    }
+
+    cout<<cnt<<"\n";
+
+    return 0;
+}
