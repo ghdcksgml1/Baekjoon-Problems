61e4be71b93958f043029b92553e90aa24699104
홍찬희 <ghdcksgml2@naver.com>
2022-11-08 23:10:29 +0900

    [Bronze IV] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,29 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string s; cin>>s;
+
+    cout<<":fan::fan::fan:\n";
+    cout<<":fan::"<<s<<"::fan:\n";
+    cout<<":fan::fan::fan:\n";
+
+    return 0;
+}
