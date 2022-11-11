dd11c2bd39a8c5a46fd62bf0726b66cdab8c4938
홍찬희 <ghdcksgml2@naver.com>
2022-11-12 01:16:12 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,31 @@
+// 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h> // memset
+
+#define ALL(x) begin(x), end(x)
+#define pll pair<ll,ll>
+#define pii pair<int,int>
+#define xx first
+#define yy second
+#define MAX 1e6
+
+using namespace std;
+typedef long long ll;
+
+int main(void){
+    // 입출력 속도 최적화
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int a,b; cin>>a>>b;
+
+    if(a == 2 && b == 18) cout<<"Special";
+    else if(a == 2 && b > 18) cout<<"After";
+    else if(a > 2) cout <<"After";
+    else cout <<"Before";
+    
+
+    return 0;
+}
