6f2f7380a5fa9af8f350098ecec6c979d4efbbd2
홍찬희 <ghdcksgml2@naver.com>
2023-09-18 13:37:48 +0900

    [Gold IV] Title: 탈출, Time: 0 ms, Memory: 2176 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.cc
+++ 백준/Gold/3055. 탈출/탈출.cc
@@ -1,8 +1,8 @@
 // 언어 : C++ , (성공/실패) : 1/0 , 메모리 : 2024 KB , 시간 : 0ms
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <algorithm>
+#include <queue>
 #include <string.h> // memset
 
 #define ALL(x) begin(x), end(x)
@@ -15,97 +15,77 @@
 using namespace std;
 typedef long long ll;
 
+struct Node {
+    int x;
+    int y;
+    char c;
+};
+
 int dx[4] = {1,-1,0,0};
 int dy[4] = {0,0,1,-1};
 
-vector<string> v;
-vector<vector<int>> visited;
-queue<pair<int,int>> q;
-queue<pair<int,int>> qq;
-
-queue<pair<int,int>> cpy_q;
-queue<pair<int,int>> cpy_qq;
+vector<string> graph;
+int dist[51][51];
+pii S;
+pii D;
+vector<pii> water;
 
 int main(void){
     // 입출력 속도 최적화
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
-    int R,C; cin>>R>>C;
+    int R, C;
+    cin >> R >> C;
 
+    for (int i=0; i<R; i++) {
+        string tmp;
+        cin >> tmp;
+        graph.push_back(tmp);
 
-
-    for(int i=0; i<R; i++){
-        string s; cin>>s;
-        vector<int> vv(C, 0);
-        visited.push_back(vv);
-        v.push_back(s);
-        for(int j=0; j<s.size(); j++){
-            if(s[j] == '*') {
-                qq.push({i,j});
-                visited[i][j] = 1;
-            }
+        for (int j=0; j<C; j++) {
+            if (tmp[j] == 'S') S = {i,j};
+            if (tmp[j] == 'D') D = {i,j};
+            if (tmp[j] == '*') water.push_back({i,j});
         }
     }
 
-    for(int i=0; i<R; i++){
-        for(int j=0; j<C; j++){
-            if(v[i][j] == 'S'){
-                q.push({i,j});
-                visited[i][j] = 1;
+    queue<Node> Q;
+    Q.push({S.first, S.second, 'S'});
+    for (pii w : water) {
+        Q.push({w.first, w.second, '*'});
+    }
+
+    while (!Q.empty()) {
+        auto cur = Q.front(); Q.pop();
+
+        if (cur.x == D.first && cur.y == D.second) {
+            cout << dist[cur.x][cur.y] << '\n';
+            return 0;
+        }
+
+        for (int k=0 ; k<4; k++) {
+            int nx = cur.x + dx[k];
+            int ny = cur.y + dy[k];
+
+            if (nx < 0 || nx >= R || ny < 0 || ny >= C) continue;
+            if (cur.c == 'S') {
+                if (graph[cur.x][cur.y] != 'S') continue;
+                if (graph[nx][ny] == '*' || graph[nx][ny] == 'X' || graph[nx][ny] == 'S') continue;
+                graph[nx][ny] = 'S';
+                dist[nx][ny] = dist[cur.x][cur.y] + 1;
+                Q.push({nx, ny, 'S'});
+
+            } else if (cur.c == '*') {
+                if (graph[nx][ny] == 'X' || graph[nx][ny] == '*' || graph[nx][ny] == 'D') continue;
+                graph[nx][ny] = '*';
+                Q.push({nx, ny, '*'});
             }
+
         }
     }
 
-    int cnt = 0;
-    while(!q.empty()){
-        cpy_q = q;
-        cpy_qq = qq;
-
-        while(q.size() > 0) q.pop();
-        while(qq.size() > 0) qq.pop();
-        while(cpy_qq.size() > 0){
-            auto cur = cpy_qq.front(); cpy_qq.pop();
-//            cout<<cur.xx<<" "<<cur.yy<<"\n";
-            for(int i=0; i<4; i++){
-                int curX = cur.xx + dx[i];
-                int curY = cur.yy + dy[i];
-
-                if(curX < 0 || curX >= R || curY < 0 || curY >= C) continue;
-                if(v[curX][curY] == 'X') continue;
-                if(v[curX][curY] == 'D') continue;
-                if(visited[curX][curY] == -1) continue;
-
-                visited[curX][curY] = -1;
-                qq.push({curX, curY});
-            }
-        }
-
-        while(cpy_q.size() > 0) {
-            auto cur = cpy_q.front();
-            cpy_q.pop();
-            if (v[cur.xx][cur.yy] == 'D') {
-                cout << cnt << "\n";
-                return 0;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int curX = cur.xx + dx[i];
-                int curY = cur.yy + dy[i];
-
-                if (curX < 0 || curX >= R || curY < 0 || curY >= C) continue;
-                if (v[curX][curY] == 'X') continue;
-                if (visited[curX][curY] != 0) continue;
-
-                visited[curX][curY] = visited[cur.xx][cur.yy] + 1;
-                q.push({curX, curY});
-            }
-        }
-
-
-        cnt++;
-    }
-    cout<<"KAKTUS";
+    cout << "KAKTUS" << '\n';
 
     return 0;
 }
